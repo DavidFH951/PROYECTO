8c342753108e63ea990de32dbca0f0388bc1ad9e
David <davidfloreshuerta951@gmail.com>
2026-08-29 01:34:37 -0500

    feat: creacion automatica de superusuario davik951

--- build.sh
+++ build.sh
@@ -5,3 +5,6 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate --no-input
+
+# Crear superusuario automáticamente si no existe
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='davik951').exists() or User.objects.create_superuser('davik951', 'admin@galeno.com', 'davidfh159')"
