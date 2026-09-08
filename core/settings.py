cdc879a113477af969088038f2038dc9f242232a
David <davidfloreshuerta951@gmail.com>
2026-09-08 14:12:34 -0500

    feat(evaluaciones): cierre forzado docente con autoenvio sincronizado para alumnos

--- core/settings.py
+++ core/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "core.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "academia/templates"],
+        "DIRS": [BASE_DIR / 'academia' / 'templates',],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
