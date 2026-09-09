92111bfa49f856984ee15b0ffa8cbade3a3ce940
David <davidfloreshuerta951@gmail.com>
2026-09-09 16:31:15 -0500

    feat(evaluaciones): Actualizando

---
+++ academia/context_processors.py
@@ -0,0 +1,16 @@
+# academia/context_processors.py
+from .models import Curso # o tu función es_docente_valido
+
+def roles_intranet(request):
+    if not request.user.is_authenticated:
+        return {'es_docente_global': False}
+    
+    # Comprobación directa si es docente
+    es_doc = (
+        request.user.is_superuser or 
+        request.user.is_staff or 
+        Curso.objects.filter(docentes=request.user).exists()
+    )
+    return {
+        'es_docente_global': es_doc
+    }
